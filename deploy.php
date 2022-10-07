520a6b5837b13af25fc712cce14a852053ec4c15
jim winstead <jimw@trainedmonkey.com>
2022-10-07 14:00:28 -0700

    Update for Deployer 7

--- deploy.php
+++ deploy.php
@@ -1,8 +1,8 @@
 <?php
 namespace Deployer;
 
-require 'recipe/common.php';
-require 'vendor/deployer/recipes/recipe/phinx.php';
+require 'recipe/composer.php';
+require 'contrib/phinx.php';
 
 // Project name
 set('application', 'talapoin');
@@ -11,27 +11,10 @@ set('application', 'talapoin');
 set('repository', 'https://github.com/jimwins/talapoin.git');
 
 // Host(s)
-inventory('hosts.yml');
+import('hosts.yml');
 
 // Tasks
-desc('Deploy your project');
-task('deploy', [
-    'deploy:info',
-    'deploy:prepare',
-    'deploy:lock',
-    'deploy:release',
-    'deploy:update_code',
-    'deploy:shared',
-    'deploy:writable',
-    'deploy:vendors',
-    'deploy:clear_paths',
-    'deploy:symlink',
-    'deploy:unlock',
-    'cleanup',
-    'success'
-]);
+after('deploy:cleanup', 'phinx:migrate');
 
-after('cleanup', 'phinx:migrate');
-
-// [Optional] If deploy fails automatically unlock.
+// If deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
