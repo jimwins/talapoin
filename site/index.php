749645a8d3f1c380d1d56cbd5d89f3a565a552d1
jim winstead <jimw@trainedmonkey.com>
2022-10-07 17:20:07 -0700

    Need /info before /.* route

--- site/index.php
+++ site/index.php
@@ -537,6 +537,17 @@ $app->post('/~webhook/post-entry',
   return $response->withStatus(200, "Success.");
 });
 
+/* DEBUG only */
+if ($DEBUG) {
+  $app->get('/info',
+            function (Request $request, Response $response) {
+              ob_start();
+              phpinfo();
+              $response->getBody()->write(ob_get_clean());
+              return $response;
+            })->setName('info');
+}
+
 /* Default for everything else (pages, redirects) */
 $app->get('/{path:.*}', function (Request $request, Response $response, $args) {
   $path= $args['path'];
@@ -571,17 +582,6 @@ $app->get('/{path:.*}', function (Request $request, Response $response, $args) {
 
   throw new \Slim\Exception\HttpNotFoundException($request, $response);
 });
-
-/* DEBUG only */
-if ($DEBUG) {
-  $app->get('/info',
-            function (Request $request, Response $response) {
-              ob_start();
-              phpinfo();
-              $response->getBody()->write(ob_get_clean());
-              return $response;
-            })->setName('info');
-}
 
 $app->run();
 
