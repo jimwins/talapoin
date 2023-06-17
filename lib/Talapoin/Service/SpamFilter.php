56ff1b182098820b90b2f2ef5744a7421886af9f
jim winstead <jimw@trainedmonkey.com>
2023-06-17 16:11:38 -0700

    Add SpamFilter service

---
+++ lib/Talapoin/Service/SpamFilter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Talapoin\Service;
+
+class SpamFilter {
+  private $config;
+
+  public function __construct(Config $config) {
+    $this->config= @$config['spam'];
+  }
+
+  public function isSpam($comment, $request) {
+    if (!$this->config) {
+      error_log("No spam config, so not checking");
+      return;
+    }
+
+    $api= new \Akismet\API($this->config['akismet_key'], $this->config['akismet_blog']);
+
+    if (@$config['akismet_debug']) {
+      $comment['is_test']= true;
+    }
+
+    return $api->isSpam($comment, $request);
+  }
+}
