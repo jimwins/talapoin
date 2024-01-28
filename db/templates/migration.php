d94097c8f0d004089fbb50408cb38c9bff5b25a3
jim winstead <jimw@trainedmonkey.com>
2024-01-27 17:08:46 -0800

    Add our own Phinx migration template
    This makes Phinx\Db\Adapter\MysqlAdapter available by default.

---
+++ db/templates/migration.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+$namespaceDefinition
+use $useClassName;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+final class $className extends $baseClassName
+{
+    public function change(): void
+    {
+
+    }
+}
