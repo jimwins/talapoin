14b9157644adf8b19223072aa052496d6ece96c5
jim winstead <jimw@trainedmonkey.com>
2024-06-15 16:56:36 -0700

    Add externalUrl method to Comment model

--- lib/Talapoin/Model/Comment.php
+++ lib/Talapoin/Model/Comment.php
@@ -6,6 +6,14 @@ namespace Talapoin\Model;
 
 class Comment extends \Talapoin\Model
 {
+    /* For the URL to refer to something external-ish, even if that breaks. */
+    public function externalUrl() {
+        if preg_match('/^(ftp|gopher|https?|mailto):/i', $this->url) {
+            return $this->url;
+        }
+        return 'https://' . $this->url;
+    }
+
     public function entry()
     {
         return $this->belongs_to('Entry')->find_one();
